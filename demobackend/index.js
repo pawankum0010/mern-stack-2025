2604b8eacf3fdcd00a84c85c0dc33c7d23ca6147
pawankexeire <pawank@exeire.com>
2026-01-04 13:08:24 +0530

    5

--- demobackend/index.js
+++ demobackend/index.js
@@ -69,7 +69,7 @@ app.use(
 // CORS Configuration
 app.use(
     cors({
-        origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+        origin: '*',
         credentials: true,
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
         allowedHeaders: ['Content-Type', 'Authorization'],
